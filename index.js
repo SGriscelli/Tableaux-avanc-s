a4cea60b55112843516a237ca8c469e27b8b7868
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-10-21 00:55:57 +0200

    Cette fois c'est bon j'espère :(

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
 const instructors = [
   {
-    name: 'John',
-    availability: 'all',
-    specialities: ['Javascript', 'Python', 'C++'],
+    name: "John",
+    availability: "all",
+    specialities: ["Javascript", "Python", "C++"],
   },
   {
-    name: 'Mary',
-    availability: 'weekend',
-    specialities: ['Javascript', 'Ruby', 'C++'],
+    name: "Mary",
+    availability: "weekend",
+    specialities: ["Javascript", "Ruby", "C++"],
   },
   {
-    name: 'Chris',
-    availability: 'evenings',
-    specialities: ['Javascript'],
+    name: "Chris",
+    availability: "evenings",
+    specialities: ["Javascript"],
   },
   {
-    name: 'Anthony',
-    availability: 'all',
-    specialities: ['Python', 'Ruby'],
+    name: "Anthony",
+    availability: "all",
+    specialities: ["Python", "Ruby"],
   },
   {
-    name: 'Pauline',
-    availability: 'only Mondays',
-    specialities: ['Javascript', 'Html', 'CSS'],
+    name: "Pauline",
+    availability: "only Mondays",
+    specialities: ["Javascript", "Html", "CSS"],
   },
   {
-    name: 'Mark',
-    availability: 'all',
-    specialities: ['C#', 'C++', 'Javascript'],
+    name: "Mark",
+    availability: "all",
+    specialities: ["C#", "C++", "Javascript"],
   },
   {
-    name: 'Helen',
-    availability: 'evenings',
-    specialities: ['Python', 'C++'],
+    name: "Helen",
+    availability: "evenings",
+    specialities: ["Python", "C++"],
   },
   {
-    name: 'Charles',
-    availability: 'none',
-    specialities: ['Python'],
+    name: "Charles",
+    availability: "none",
+    specialities: ["Python"],
   },
 ];
 
-
-const jsWeekendInstructors = instructors.filter(instructor =>
-  instructor.specialities.includes('Javascript') &&
-  (instructor.availability === 'weekend' || instructor.availability === 'all')
+const jsWeekendInstructors = instructors.filter(
+  (instructor) =>
+    instructor.specialities.includes("Javascript") &&
+    (instructor.availability === "weekend" || instructor.availability === "all")
 );
 
+jsWeekendInstructors.forEach((instructor) => {
+  let message = `Hi ${instructor.name}, we inform you that this weekend you will be doing the support class.`;
 
-jsWeekendInstructors.forEach(instructor => {
-  console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class`);
-});
-
-
-jsWeekendInstructors.forEach(instructor => {
-  if (instructor.specialities.includes('Python')) {
-    console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop`);
+  if (instructor.specialities.includes("Python")) {
+    message += ` You also need to prepare a Python workshop.`;
   }
+
+  console.log(message);
 });
