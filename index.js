69f3828c49c2ee628e280c1210402c783186356b
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-10-20 19:39:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 const instructors = [
-    { name: "Alice", availability: "weekend", specialties: ["JavaScript", "HTML"] },
-    { name: "Bob", availability: "all", specialties: ["Python", "JavaScript"] },
-    { name: "Charlie", availability: "weekday", specialties: ["Java"] },
-    { name: "David", availability: "weekend", specialties: ["JavaScript", "Python"] },
-    { name: "Eve", availability: "all", specialties: ["C#", "JavaScript"] }
+  {
+    name: 'John',
+    availability: 'all',
+    specialities: ['Javascript', 'Python', 'C++'],
+  },
+  {
+    name: 'Mary',
+    availability: 'weekend',
+    specialities: ['Javascript', 'Ruby', 'C++'],
+  },
+  {
+    name: 'Chris',
+    availability: 'evenings',
+    specialities: ['Javascript'],
+  },
+  {
+    name: 'Anthony',
+    availability: 'all',
+    specialities: ['Python', 'Ruby'],
+  },
+  {
+    name: 'Pauline',
+    availability: 'only Mondays',
+    specialities: ['Javascript', 'Html', 'CSS'],
+  },
+  {
+    name: 'Mark',
+    availability: 'all',
+    specialities: ['C#', 'C++', 'Javascript'],
+  },
+  {
+    name: 'Helen',
+    availability: 'evenings',
+    specialities: ['Python', 'C++'],
+  },
+  {
+    name: 'Charles',
+    availability: 'none',
+    specialities: ['Python'],
+  },
 ];
 
+
 const jsWeekendInstructors = instructors.filter(instructor =>
-    instructor.specialties.includes("JavaScript") &&
-    (instructor.availability === "weekend" || instructor.availability === "all")
+  instructor.specialities.includes('Javascript') &&
+  (instructor.availability === 'weekend' || instructor.availability === 'all')
 );
 
+
 jsWeekendInstructors.forEach(instructor => {
-    if (instructor.specialties.includes("Python")) {
-        console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop`);
-    } else {
-        console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class`);
-    }
+  console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class`);
+});
+
+
+jsWeekendInstructors.forEach(instructor => {
+  if (instructor.specialities.includes('Python')) {
+    console.log(`Hi ${instructor.name}, we inform you that this weekend you will be doing the support class and you need to prepare a Python workshop`);
+  }
 });
